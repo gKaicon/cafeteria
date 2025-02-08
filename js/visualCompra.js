9a9fa62e00aa71eada5a037c5bbb5dcd63bb065f
Gabriel Káicon <gkaicon2@gmail.com>
2025-02-08 19:30:24 -0300

    Finalização de todas as tarefas, corrigir apenas o CRUD do Produto

--- js/visualCompra.js
+++ js/visualCompra.js
@@ -1,4 +1,3 @@
-
 carregarTabela()
 
 document.querySelector('#addItensCompra').addEventListener('click', (event) => {
@@ -74,64 +73,3 @@ function verItensCompra(idCompra){
             tabela.innerHTML = dados.html
         })
 }
-
-// //carregar dados para Editar
-// function editarPessoa(id) {
-//     document.getElementById('codigo').value = id
-//     fetch('include/___/Controlador.php?acao=getByID', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//             id: id
-//         })
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             document.getElementById('').value = data[0]._
-
-//             document.getElementById('enviar').style.display = 'none'
-//             document.getElementById('salvar').style.display = 'block'
-//         })
-// }
-
-
-// //atualizar
-// document.querySelector("#salvar").addEventListener('click', (event) => {
-//     event.preventDefault();
-//     let id = document.getElementById('id').value
-//     let dados = {
-//         _: document.getElementById('').value,
-//     }
-
-//     fetch('include/___/Controlador.php?acao=update&id=' + id + '', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(dados)
-//     })
-//         .then(response => {
-//             if (!response.ok) throw new Error('Network response was not ok. Error:' + response);
-//         })
-//         .then(data => { console.log('Success:', data); })
-//         .catch(error => { console.error('Error:', error); });
-//     carregarTabela()
-// })
-
-// //deletar
-// function deletarPessoa(id) {
-//     if (window.confirm("Tem certeza que deseja apagar?")) {
-//         fetch('include/___/Controlador.php?acao=delete&id=' + id + '', {
-//         }).then(response => {
-//             if (!response.ok)
-//                 throw new Error('Network response was not ok. Error:' + response);
-//         }).then(data => {
-//             console.log('Success:', data);
-//         }).catch(error => {
-//             console.error('Error:', error);
-//         });
-//         carregarTabela()
-//     }
-// }
