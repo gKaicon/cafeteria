fbcc2be1842870f88b6c209d09dc12beb050e14d
Gabriel Káicon <gkaicon2@gmail.com>
2025-02-09 22:33:48 -0300

    Correção no logout

--- pages/include/check.php
+++ pages/include/check.php
@@ -2,7 +2,6 @@
 session_start();
 $response;
 $response["logado"] = ($_SESSION['logado'] ? "true" : "false");
-$response["tipoUser"] = ((($_SESSION['tipo'] == 'admin') || ($_SESSION['tipo'] == 'default')) ? 0 : 1);
 
 echo json_encode($response);
 ?>
